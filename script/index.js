03e76044777c235857b1a012d3cf69615a4b5695
Erik Ryman <rymanerik@live.se>
2023-10-14 10:53:38 +0200

    Temporary push to save changes, no pull request until finished

--- script/index.js
+++ script/index.js
@@ -25,7 +25,35 @@ const smallPortionSvg =
   '<svg class="portionIcon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>';
 const largePortionSvg =
   '<svg class="portionIcon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"/></svg>';
-
+// SVG for adding / removing items in basket
+const increaseQuantity = `<svg
+    class="basketQuantity increaseQuantity addToBasketSmall addToBasket"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+    />
+  </svg>`;
+const decreaseQuantity = `<svg
+    class="basketQuantity decreaseQuantity"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+    />
+  </svg>`;
 //Setting default values
 let language = true;
 let selectedSort = "standard";
@@ -44,7 +72,7 @@ const displayFoods = function (foods) {
     let priceHtml = "";
 
     if (food.price.length > 1) {
-      priceHtml = `<p class="food-price">${smallPortionSvg} ${food.price[0]} kr <button class="addToBasketSmall">${orderLanguage}</button></p><p>${largePortionSvg} ${food.price[1]} kr <button class="addToBasketLarge">${orderLanguage}</button></p>`;
+      priceHtml = `<p class="food-price">${smallPortionSvg} ${food.price[0]} kr <button class="addToBasketSmall addToBasket">${orderLanguage}</button></p><p>${largePortionSvg} ${food.price[1]} kr <button class="addToBasketLarge addToBasket">${orderLanguage}</button></p>`;
     } else {
       priceHtml = `<p class="food-price">${largePortionSvg} ${food.price[0]} kr <button class="addToBasket">${orderLanguage}</button></p>`;
     }
@@ -131,36 +159,101 @@ const sortFoodByPrice = function (filteredMenu) {
   }
 };
 
-// Adding items to the basket
-foodDiv.addEventListener("click", function (event) {
+const addRemoveFromBasket = function (event) {
   const target = event.target; // Gets the target you clicked
 
   // Checks if you clicked an actual button
-  if (
-    target.classList.contains("addToBasket") ||
-    target.classList.contains("addToBasketLarge") ||
-    target.classList.contains("addToBasketSmall")
-  ) {
+  if (target.classList.contains("addToBasket")) {
     // Finds the div containing food details
     const foodContainer = target.closest("div");
     // Checks if foodContainer is a truthy value (if the div was found it is truthy)
     if (foodContainer) {
-      let icon = largePortionSvg; // Icon to represent large or small portion in basket
+      let portion = ""; // Default for large portion
+      let quantity = 1;
+      let menuItem;
+
       const name = foodContainer.querySelector(".food-title").textContent; // Gets the food name
-      let priceElement = foodContainer.querySelector(".food-price"); // Gets the food price
+      let price = 0;
+
       if (target.classList.contains("addToBasketSmall")) {
-        icon = smallPortionSvg; // Change default (lage portion) to small portion icon
-      }
-      if (target.classList.contains("addToBasketLarge")) {
+        portion = language ? "(liten)" : "(small)"; // Changes language of small portion
+        menuItem = menu.find(
+          (item) => item.seName === name || item.enName === name
+        );
+        if (menuItem) {
+          price = menuItem.price[0];
+        }
+      } else if (target.classList.contains("addToBasketLarge")) {
         // Alters price based on the button clicked
         // If price has multiple options, use the second price in the array
-        priceElement = priceElement.nextElementSibling;
+        // Access the large portion price from the menu
+        menuItem = menu.find(
+          (item) => item.seName === name || item.enName === name
+        );
+        if (menuItem) {
+          price = menuItem.price[1];
+        }
+      } // Access the price for the default portion
+      else {
+        menuItem = menu.find(
+          (item) => item.seName === name || item.enName === name
+        );
+        if (menuItem) {
+          price = menuItem.price[0];
+        }
       }
-      const priceText = priceElement.textContent;
-      const price = parseInt(priceText.match(/\d+/)); // Extract the price using regular expression that ckecks for one or more consecutive digits and then parses them to integers.
+      // Checks if item is already in basket, if not ads the food item to the basket
+      if (
+        basket.find((item) => item.name === name && item.portion === portion)
+      ) {
+        const existingItem = basket.find(
+          (item) => item.name === name && item.portion === portion
+        );
+        existingItem.quantity += 1;
+        existingItem.price += price;
+      } else {
+        basket.push({ name, price, portion, quantity, menuItem });
+      }
 
-      // Ads the food item to the basket
-      basket.push({ name, price, icon });
+      updateOrderList();
+      updateOrderSummary();
+    }
+  }
+};
+
+// Adding items to the basket
+foodDiv.addEventListener("click", addRemoveFromBasket);
+
+orderList.addEventListener("click", function (event) {
+  const target = event.target; // Gets the target you clicked
+  // Checks if you clicked an actual button
+  if (target.classList.contains("basketQuantity")) {
+    // Finds the div containing food details
+    const basketContainer = target.closest("div");
+    // Checks if foodContainer is a truthy value (if the div was found it is truthy)
+    if (basketContainer) {
+      const name = basketContainer.querySelector(".foodName").textContent; //basketContainer.querySelector(".foodName").textContent; // Gets the food name
+      // if (basketContainer.querySelector(".foodName"))
+      const portion = basketContainer.querySelector(".foodPortion").textContent;
+      // Checks if item is already in basket, if not ads the food item to the basket
+      const existingItem = basket.find((item) => item.name === name);
+
+      const originalItem = menu.find(
+        (item) => item.seName === name || item.enName === name
+      );
+
+      const originalPrice = originalItem.price[0];
+      if (
+        // basket.find((item) => item.name === name && item.portion === portion)
+        target.classList.contains("increaseQuantity")
+      ) {
+        existingItem.quantity += 1;
+        existingItem.price += originalPrice;
+      } else if (target.classList.contains("decreaseQuantity")) {
+        console.log("decrease");
+        existingItem.quantity -= 1;
+        existingItem.price -= originalPrice;
+      }
       updateOrderList();
       updateOrderSummary();
     }
@@ -168,24 +261,35 @@ foodDiv.addEventListener("click", function (event) {
 });
 
 function updateOrderList() {
+  localStorage.setItem("basket", JSON.stringify(basket));
   orderList.innerHTML = "";
-  basket.forEach((item) => {
-    const html = `<li class="basketItem">${item.name} ${item.icon}</li>`;
-    orderList.insertAdjacentHTML("beforeend", html);
-    console.log(item);
-  });
+  const lang = language ? "seName" : "enName"; // access translated names
+  basket
+    .filter((item) => item.quantity > 0)
+    .forEach((item, i) => {
+      const name = item.menuItem[lang];
+      const html = `<li class="basketItem">${increaseQuantity} ${item.quantity} ${decreaseQuantity} <span class="foodName">${name}</span> <span class="foodPrice">${item.price}</span> kr <span class="foodPortion">${item.portion}</span></li>`;
+      orderList.insertAdjacentHTML("beforeend", html);
+    });
 }
 
 function updateOrderSummary() {
   orderSummary.innerHTML = "";
+  let sum = language ? "Summa" : "Sum";
   const total = basket.reduce((sum, item) => sum + item.price, 0); //Starts at 0 and accumulates the price
-  const html = `Summa: ${total} kr`;
+  const html = `${sum}: ${total} kr`;
   orderSummary.insertAdjacentHTML("beforeend", html);
 }
 
 if (localStorage.getItem("selectedLanguage") === "en") {
   language = false;
 } else language = true;
+
+if (localStorage.getItem("basket")) {
+  basket = JSON.parse(localStorage.getItem("basket"));
+  updateOrderList();
+  updateOrderSummary();
+}
 
 // Event listeners - calls filterFoods to update the list of foods -------------------------------------------
 checkGlutenFree.addEventListener("change", filterFoods);
@@ -200,6 +304,8 @@ languageSelect.forEach((change) =>
   change.addEventListener("change", function () {
     language = !language; // Switch from default Swedish to English
     filterFoods();
+    updateOrderList();
+    updateOrderSummary();
   })
 );
 
